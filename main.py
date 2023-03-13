803a40e54bbd6679d1703319fca0733a5afcc4eb
Alex55550000 <106884796+Alex55550000@users.noreply.github.com>
2023-03-13 22:23:20 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,34 @@
+import os
+from debug_utils import logger
+
+
+def test_1():
+    path = 'mainlog.txt'
+    if os.path.exists(path):
+        os.remove(path)
+
+
+@logger(path='mainlog.txt')
+def hello_world():
+    return 'Hello World'
+
+
+@logger(path='mainlog.txt')
+def summator():
+    a = int(input('Enter your two numbers for sum:\n'))
+    b = int(input())
+    return f'Your two numbers are --> {a} and {b}\nSum --> {a + b}'
+
+
+@logger(path='mainlog.txt')
+def div():
+    a = int(input('Enter your two numbers for division:\n'))
+    b = int(input())
+    return f'Your two numbers are --> {a} and {b}\nDivision --> {a / b}'
+
+
+if __name__ == '__main__':
+    test_1()
+    hello_world()
+    summator()
+    div()
